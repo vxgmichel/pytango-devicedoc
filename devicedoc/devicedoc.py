7ae83e8bb33ca81341a86cc1e3773a3a5aaa0c22
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-05-28 13:31:06 +0200

    flake8ify

--- devicedoc/devicedoc.py
+++ devicedoc/devicedoc.py
@@ -2,9 +2,8 @@
 from a HLAPI Tango Device class."""
 
 # Imports
-from mock import Mock
 from sphinx.application import Sphinx
-from sphinx.ext.autodoc import ClassDocumenter, AttributeDocumenter 
+from sphinx.ext.autodoc import ClassDocumenter, AttributeDocumenter
 from sphinx.ext.autodoc import ClassLevelDocumenter
 from collections import defaultdict
 
@@ -12,11 +11,12 @@ from collections import defaultdict
 # Mock
 class BaseMock(object):
     """Mocking base class."""
-    
+
     def __init__(self, func=None, **kwargs):
         """Save kwargs and function documentation."""
         self.kwargs = kwargs
-        self.func_doc = func.__doc__ if func else None 
+        self.func_doc = func.__doc__ if func else None
+        self.__tango_command__ = True
 
     def __call__(self, func):
         """Decorator support."""
@@ -35,7 +35,7 @@ class BaseMock(object):
         """Generate a readable representation."""
         args = []
         # Add type
-        name = type(self).__name__.replace('_',' ')
+        name = type(self).__name__.replace('_', ' ')
         base = "{}:\n".format(name.capitalize())
         # Add kwargs
         for key, value in sorted(self.kwargs.items()):
@@ -48,6 +48,8 @@ class BaseMock(object):
             except AttributeError:
                 pass
             args.append("    - {0} : {1}".format(key, value))
+        if not args:
+            return base[:-2] + '.'
         return base + '\n'.join(args)
 
     def get_doc(self):
@@ -58,17 +60,29 @@ class BaseMock(object):
 
 
 # Tango mock
-class device_property(BaseMock): pass
-class attribute(BaseMock): pass
-class command(BaseMock): pass
-class DeviceMeta(type): pass
+
+class device_property(BaseMock):
+    pass
+
+
+class attribute(BaseMock):
+    pass
+
+
+class command(BaseMock):
+    pass
+
+
+class DeviceMeta(type):
+    pass
+
+
 class Device(object):
     __metaclass__ = DeviceMeta
 
 
 # Monkey patching
 def pytango_patch():
-    from types import MethodType
     from PyTango import server
     server.attribute = attribute
     server.command = command
@@ -106,7 +120,7 @@ class TangoDeviceDocumenter(ClassDocumenter):
             self.indent = temp
         # Generate documentation
         ClassDocumenter.generate(self, more_content, real_modname,
-                                      check_module, all_members)
+                                 check_module, all_members)
 
     def filter_members(self, members, want_all):
         """Filter to keep only objects of valid types."""
@@ -163,14 +177,14 @@ class TangoItemDocumenter(ClassLevelDocumenter):
         # Generate documentation
         ClassLevelDocumenter.generate(self, more_content, real_modname,
                                       check_module, all_members)
-       
+
     def get_doc(self, encoding=None, ignore=1):
         """Patch to get the docs from the mock object."""
         NL = '\n'
         MU = ' |'
         obj_repr = repr(self.object).replace(NL, NL+MU) + NL
         obj_doc = self.object.get_doc() + NL
-        return [obj_repr.split(NL), obj_doc.split(NL)] 
+        return [obj_repr.split(NL), obj_doc.split(NL)]
 
     def add_content(self, more_content, no_docstring=False):
         """Patch to add the documentation from the mock object
@@ -179,7 +193,7 @@ class TangoItemDocumenter(ClassLevelDocumenter):
         for i, line in enumerate(self.process_doc(docstrings)):
             self.add_line(line, '<autodoc>', i)
         ClassLevelDocumenter.add_content(self, more_content, True)
-        
+
 
 # Tango property documenter
 class TangoPropertyDocumenter(TangoItemDocumenter):
@@ -188,7 +202,6 @@ class TangoPropertyDocumenter(TangoItemDocumenter):
     section = "Device properties"
     types = [device_property]
     member_order = 70
-    
 
 
 # Tango attribute documenter
@@ -208,11 +221,11 @@ class TangoCommandDocumenter(TangoItemDocumenter):
     types = [command]
     member_order = 90
 
-    
+
 # Setup the sphinx extension
 def setup(app):
     """Sphinx extension setup function."""
-    if not isinstance(app, Sphinx): 
+    if not isinstance(app, Sphinx):
         return
     pytango_patch()
     app.add_autodocumenter(TangoDeviceDocumenter)
