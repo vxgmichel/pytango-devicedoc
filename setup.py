bdd16356d3a28f0eca58e88f8003d03a85e2fbc0
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-06-10 16:25:24 +0200

    bump version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = "python-devicedoc",
-      version = "0.1.1",
+      version = "0.1.2",
       description = "Sphinx extension for Tango devices documentation.",
       packages = ['devicedoc'],
       include_package_data=True,
